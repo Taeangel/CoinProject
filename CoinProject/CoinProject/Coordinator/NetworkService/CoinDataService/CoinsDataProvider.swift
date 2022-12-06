6cf8bd013edd1056b94b1459ba6745ca29b7953f
Taeangel <wngks2050@gmail.com>
2022-12-07 04:28:14 +0900

    refactor: coingeckoUrl연산 프로퍼티로  강제언랩핑 변경

--- CoinProject/CoinProject/Coordinator/NetworkService/CoinDataService/CoinsDataProvider.swift
+++ CoinProject/CoinProject/Coordinator/NetworkService/CoinDataService/CoinsDataProvider.swift
@@ -9,10 +9,16 @@ import Foundation
 import Combine
 
 extension Provider {
+  var coingeckoUrl: URL {
+    guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else {
+      return URL(fileURLWithPath: "")
+    }
+    return url
+  }
+  
   func getCoins() -> AnyPublisher<[CoinModel], CoinError> {
-   let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h")
     
-    var request = URLRequest(url: url!)
+    var request = URLRequest(url: coingeckoUrl)
     request.httpMethod = "GET"
 
     return requestPublisher(request)
