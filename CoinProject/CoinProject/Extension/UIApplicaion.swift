b8e2214f6be4d1e203245d98840eea94a45e3744
Taeangel <wngks2050@gmail.com>
2022-12-08 05:45:53 +0900

    feat: UIApplication 확장

---
+++ CoinProject/CoinProject/Extension/UIApplicaion.swift
@@ -0,0 +1,25 @@
+//
+//  UIApplicaion.swift
+//  CoinProject
+//
+//  Created by song on 2022/12/08.
+//
+
+import Foundation
+import SwiftUI
+ 
+extension UIApplication {
+    func hideKeyboard() {
+        guard let window = windows.first else { return }
+        let tapRecognizer = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
+        tapRecognizer.cancelsTouchesInView = false
+        tapRecognizer.delegate = self
+        window.addGestureRecognizer(tapRecognizer)
+    }
+ }
+ 
+extension UIApplication: UIGestureRecognizerDelegate {
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return false
+    }
+}
